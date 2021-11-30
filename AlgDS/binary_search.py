040e08ad311b98d0b2fbc26cf90876b1953f6091
riccardompesce <riccardompesce@gmail.com>
2021-11-30 09:33:32 +0100

    TODO binary search

--- AlgDS/binary_search.py
+++ AlgDS/binary_search.py
@@ -1,24 +1,36 @@
+import random
+
 def binary_search(array, n):
-    if len(array) == 1:
-        return False
-    
-    mid = len(array) // 2
+    low = 0
+    high = len(array) - 1
 
-    print(mid)
-    print(len(array))
+    while low < high:
+        mid = (low + high) // 2 + 1
+        if n < array[mid]:
+            high = mid - 1
+        elif n > array[mid]:
+            low = mid + 1
+        else:
+            return mid
 
-    if n < array[mid]:
-        return binary_search(array[:mid], n)
-    elif n > array[mid]:
-        return binary_search(array[mid:], n)
-    else:
-        return True
+    return -1
+
+
+def test_binary_search(n_test_cases, min_size=10, max_size=100):
+    if min_size < 10:
+        min_size = 10
+    successes = 0
+    for _ in range(n_test_cases):
+        a = random.sample(range(max_size), random.randint(0, max_size))
+        i = random.randint(0, len(a) - 1)
+        if i == binary_search(a, a[i]):
+            successes += 1
+
+    return (successes, n_test_cases)
+
 
 def main():
-    print(binary_search([1, 2, 3, 4, 5], 4))
-    print(binary_search([1, 2, 3, 4, 5], 2))
-    print(binary_search([1, 2, 3, 4, 5], 5))
-    print(binary_search([1, 2, 3, 4, 5], 66))
+    print(test_binary_search(20))
 
 if __name__ == "__main__":
     main()
