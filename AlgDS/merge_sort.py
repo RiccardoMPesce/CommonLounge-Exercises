b5f774ca4a48eee70fdc837911c0b41430178b84
RiccardoMPesce <riccardompesce@gmail.com>
2023-11-13 17:30:27 +0100

    Tested merge sort

--- AlgDS/merge_sort.py
+++ AlgDS/merge_sort.py
@@ -1,3 +1,6 @@
+import datetime
+import random
+
 # merge_sort.py 
 def merge_sort(array):
     if len(array) <= 1:
@@ -39,3 +42,26 @@ def merge(left, right):
 array = [5, 10, 8, 7, 3, 7, 6, 12, 2, 7]
 result = merge_sort(array)
 assert result == [2, 3, 5, 6, 7, 7, 7, 8, 10, 12], result
+
+npassed, nfailed = 0, 0
+for itest in range(1, 13):
+    print('Test case:', itest)
+    nelements = random.randint(int((10**(itest/2.))/2), int(10**(itest/2.)))
+    print('Array size:', nelements)
+    array = [random.randint(1, 100*nelements) for _ in range(nelements)]
+    tic = datetime.datetime.now()
+    submission = merge_sort(array)
+    toc = datetime.datetime.now()
+    answer = sorted(array)
+    correct = len(submission) == len(answer) and (submission == answer)
+    if correct:
+        print('PASSED (Runtime = %.2f seconds)' % (toc-tic).total_seconds())
+        npassed += 1
+    else:
+        print('FAILED')
+        nfailed += 1
+        print(array)
+        print(submission)
+        print(answer)
+    print('='*100)
+print('TOTAL PASSED: %d. TOTAL FAILED: %d.' % (npassed, nfailed))
